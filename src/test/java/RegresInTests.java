13e851fcf29cbe630a93c812ddbac71abb8baec3
Andrew Bartenev <abartenev@rvision.ru>
2023-04-01 18:02:06 +0300

    added regresin tests

---
+++ src/test/java/RegresInTests.java
@@ -0,0 +1,106 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static io.restassured.http.ContentType.JSON;
+
+public class RegresInTests {
+
+    @Test
+    void singleUserFistNameTest() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.first_name", is("Janet"));
+    }
+
+    @Test
+    void createNameTest() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"));
+    }
+
+    @Test
+    void deleteTest() {
+
+        given()
+                .log().uri()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
+
+    @Test
+    void registerSuccessfulTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void loginUnsuccessfulTest() {
+        String data = "{ \"email\": \"peter@klaven\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+    @Disabled
+    @Test
+    void updateTest() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+        ZonedDateTime myZonedDateTime = ZonedDateTime.now().minusHours(3);
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("updatedAt", is(myZonedDateTime));
+    }
+}
